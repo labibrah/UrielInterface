ca7e4ca0dc921f8e89b785db20a0bce7bdf6f401
Labib Rahman <labib.rahman@northsouth.edu>
2024-12-10 02:35:11 +0600

    button

--- interface.py
+++ interface.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import random as rnd
 
-## Global settings variables
-
-## Test
-
 'Welcome to URIEL+'
 
 ## Settings button and logic to change configurations
@@ -14,6 +10,7 @@ if st.button("Settings", icon=":material/settings:"):
     left_column, middle_column, right_column = st.columns(3)
     with left_column:
         cache_toggle = st.toggle("Activate caching", help='Caching is default false to save memory, setting it true means updates to databases or imputation are saved to files')
+        distance_option = st.toggle("Set cosine distance", help='Distance metric is default "angular". The distance metric chosen is how language vectors are compared for language distance calculations')
     with middle_column:
         avg_aggregation_toggle = st.toggle("Set aggregation to average", help='Aggregation is default set to union of typological feature data across sources in URIEL+, toggling this will set aggregation to average of typological feature data across sources')
     with right_column:
@@ -39,6 +36,7 @@ This approach enhances the overall quality of typological analysis and minimizes
 
 df = pd.DataFrame({
     'calculation options': ['Calculate distance between languages','Impute the URIEL database'],
+    # 'second column': [10, 20, 30, 40]
     })
 
 option = st.selectbox(
